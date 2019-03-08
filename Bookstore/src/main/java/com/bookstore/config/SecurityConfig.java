05ac64e5b68a10005672675c72cf64dbc0ec64bd
khmu <khmu@NB-KHMU.local.parcit>
2019-03-08 16:40:18 +0100

    bookstore frontend

--- Bookstore/src/main/java/com/bookstore/config/SecurityConfig.java
+++ Bookstore/src/main/java/com/bookstore/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			"/newUser",
 			"/forgetPassword",
 			"/bookshelf",
-			"/bookDetail",
+			"/bookDetail/**",
 			"/login",
 			"/fonts/**"
 	};
